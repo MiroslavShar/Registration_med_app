131c198fa02b57ebf8daf761c7b807d4dbb30ec9
MiroslavShar <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>
2024-04-13 17:41:24 +0200

    New tests for views

--- registration/conftest.py
+++ registration/conftest.py
@@ -1,5 +1,5 @@
 import pytest
-from registration.models import Doctor, Patient, MedicalHistory, Recommendation, ReasonForVisit, Visit
+from registration.models import Doctor, Patient, MedicalHistory, Recommendation, ReasonForVisit, Visit, User
 
 @pytest.fixture
 def doctors():
@@ -7,3 +7,40 @@ def doctors():
     for x in range(5):
         doctors.append(Doctor.objects.create(name=x))
         return doctors
+
+@pytest.fixture
+def user():
+    u = User.objects.create_user(username='test5', password='test5')
+    return u
+
+@pytest.fixture
+def reasons():
+    reasons = []
+    for x in range(7):
+        reasons.append(ReasonForVisit.objects.create(reasons=x))
+        return reasons
+
+@pytest.fixture
+def patients():
+    patients = []
+    for x in range(7):
+        patients.append(Patient.objects.create(name=x, surname=x))
+        return patients
+
+
+@pytest.fixture
+def recommendations(patients, new_visit_date='2024-12-10 16:00'):
+    recommendations = []
+    for x in range(7):
+        recommendations.append(Recommendation.objects.create(recommendations=x, patient=patients[x], new_visit_date=new_visit_date))
+        return recommendations
+
+@pytest.fixture
+def visits(patients, doctors, reasons, date="2024-12-10 16:00"):
+    visits = []
+    for x in range(7):
+        v = Visit.objects.create(patient=patients[x], date=date, reason=reasons[x])
+        v.doctor.set(doctors)
+        visits.append(v)
+        return visits
+
--- registration/tests.py
+++ registration/tests.py
@@ -50,12 +50,190 @@ import pytest
 #     }
 #     response = client.post(url, data)
 #     assert response.status_code == 302
-#     Doctor.objects.get(name='Aleksander')
+#     assert response.url.startswith(reverse('home'))
+#     assert Doctor.objects.get(name='Aleksander')
+#
+#
+# @pytest.mark.django_db
+# def test_let_see_doctor(doctors):
+#     client = Client()
+#     url = reverse('show_doctors')
+#     response = client.get(url)
+#     assert response.status_code == 200
+#     assert response.context['doctors'].count() == len(doctors)
+#
+# @pytest.mark.django_db
+# def test_add_reason():
+#     client = Client()
+#     url = reverse('reason')
+#     response = client.get(url)
+#     assert response.status_code == 200
+
+# @pytest.mark.django_db
+# def test_add_empty_reason():
+#     client = Client()
+#     url = reverse('reason')
+#     data = {
+#         'reasons': ''
+#     }
+#     response = client.post(url, data)
+#     assert response.status_code == 200
+#
+# @pytest.mark.django_db
+# def test_add_not_empty_reason():
+#     client = Client()
+#     url = reverse('reason')
+#     data = {
+#         'reasons': 'L12'
+#     }
+#     response = client.post(url, data)
+#     assert response.status_code == 302
+#     assert response.url.startswith(reverse('home'))
+#     assert ReasonForVisit.objects.get(reasons='L12')
+
+# @pytest.mark.django_db
+# def test_add_patient():
+#     client = Client()
+#     url = reverse('patient')
+#     response = client.get(url)
+#     assert response.status_code == 200
+
+# @pytest.mark.django_db
+# def test_add_empty_patient():
+#     client = Client()
+#     url = reverse('patient')
+#     data = {
+#         'name': '',
+#         'surname': ''
+#     }
+#     response = client.post(url, data)
+#     assert response.status_code == 200
+#
+# @pytest.mark.django_db
+# def test_add_not_empty_patient():
+#     client = Client()
+#     url = reverse('patient')
+#     data = {
+#         'name': 'Ludovico',
+#         'surname': 'Wielki'
+#     }
+#     response = client.post(url, data)
+#     assert response.status_code == 302
+#     assert response.url.startswith(reverse('home'))
+#     assert Patient.objects.get(name='Ludovico', surname='Wielki')
+#
+# @pytest.mark.django_db
+# def test_not_login_add_med_history():
+#     client = Client()
+#     url = reverse('history')
+#     response = client.get(url)
+#     assert response.status_code == 302 #Musisz być zalogowany
+#
+# @pytest.mark.django_db
+# def test_login_add_med_history(user):
+#     client = Client()
+#     client.force_login(user)
+#     url = reverse('history')
+#     response = client.get(url)
+#     assert response.status_code == 200 #Musisz być zalogowany
+
+# @pytest.mark.django_db
+# def test_add_empty_med_history(user):
+#     client = Client()
+#     client.force_login(user)
+#     url = reverse('history')
+#     data = {
+#         'interview': '',
+#         'ophthalmological_interview': '',
+#         'visus_distance': '',
+#         'visus_near': '',
+#         'tonus': '',
+#         'anterior_segment_right_eye': '',
+#         'anterior_segment_left_eye': '',
+#         'fundus_segment_right_eye': '',
+#         'fundus_left_eye': '',
+#         'date': '',
+#         'patient': '',
+#     }
+#     response = client.post(url, data)
+#     assert response.status_code == 200
 
 @pytest.mark.django_db
-def test_let_see_doctor(doctors):
+def test_add_not_empty_med_history(user):
     client = Client()
-    url = reverse('show_doctors')
-    response = client.get(url)
-    assert response.status_code == 200
-    assert response.context['doctors'].count() == len(doctors)
+    url = reverse('reason')
+    data = {
+        'interview': 'test',
+        'ophthalmological_interview': 'test',
+        'visus_distance': 'test',
+        'visus_near': 'test',
+        'tonus': 'test',
+        'anterior_segment_right_eye': 'test',
+        'anterior_segment_left_eye': 'test',
+        'fundus_segment_right_eye': 'test',
+        'fundus_left_eye': 'test',
+        'date': '2024-12-10',
+        'patient': ''
+    }
+    response = client.post(url, data)
+    assert response.status_code == 302
+    assert response.url.startswith(reverse('home'))
+    assert ReasonForVisit.objects.get(reasons='L12')
+#
+# @pytest.mark.django_db
+# def test_show_histories():
+#     client = Client()
+#     url = reverse('show_histories')
+#     response = client.get(url)
+#     assert response.status_code == 200
+#
+# @pytest.mark.django_db
+# def test_add_recomendation():
+#     client = Client()
+#     url = reverse('recommendation')
+#     response = client.get(url)
+#     assert response.status_code == 200
+#
+# @pytest.mark.django_db
+# def test_add_visit():
+#     client = Client()
+#     url = reverse('visit')
+#     response = client.get(url)
+#     assert response.status_code == 200
+#
+# @pytest.mark.django_db
+# def test_show_reason(reasons):
+#     client = Client()
+#     url = reverse('show_reasons')
+#     response = client.get(url)
+#     assert response.status_code == 200
+#     assert response.context['reasons'].count() == len(reasons)
+#
+# @pytest.mark.django_db
+# def test_show_patients(patients):
+#     client = Client()
+#     url = reverse('show_patients')
+#     response = client.get(url)
+#     assert response.status_code == 200
+#     assert response.context['patients'].count() == len(patients)
+#
+# @pytest.mark.django_db
+# def test_show_recommendations(recommendations):
+#     client = Client()
+#     url = reverse('show_recommendation')
+#     response = client.get(url)
+#     assert response.status_code == 200
+#     assert response.context['recommendations'].count() == len(recommendations)
+#
+# @pytest.mark.django_db
+# def test_show_visit(visits):
+#     client = Client()
+#     url = reverse('show_visit')
+#     response = client.get(url)
+#     assert response.status_code == 200
+#     assert response.context['visits'].count() == len(visits)
+
+
+
+
+
